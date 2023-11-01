ffd5545d5680e3d7996c6afa766ac0cb7ad04afc
Patrik93JS <99814207+Patrik93JS@users.noreply.github.com>
2023-11-01 12:05:34 +0100

    napojeni na db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,20 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = 5500;
 
 app.get("/", (req, res) => {
 	res.send("Hello World");
 });
 
-app.listen(port, () => {
-	console.log("Server running on port: " + port);
-});
+const port = process.env.PORT;
+
+mongoose
+	.connect(process.env.MONGO_CONNECTION_STRING!)
+	.then(() => {
+		console.log("Mongoose connected");
+		app.listen(port, () => {
+			console.log("Server running on port: " + port);
+		});
+	})
+	.catch(console.error);
